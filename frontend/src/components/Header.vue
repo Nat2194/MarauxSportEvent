826c857cacd024675aec6c7da100fea0c6976006
Nat2194 <94376896+Nat2194@users.noreply.github.com>
2023-08-07 04:50:43 +0200

    header modifié et rendu responsive

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,59 +1,64 @@
 <template>
-	<div class="fixed flex flex-col top-0 w-screen bg-white z-10 shadow-md">
-		<div class="header-div justify-between flex flex-wrap py-4">
-			<div class="flex gap-2">
-				<router-link to="/" class="flex items-center gap-2 !text-black logo">
-					<span
-						class="z-10 bg-white title-size font-bold underline font-yanone-kaffeesatz">marauXsporTevenT</span>
-				</router-link>
-			</div>
-			<div class="flex gap-8 items-center">
-				<ul id="header-menu" class="flex gap-4 button-header-size">
-					<li class="icon-slash">
-						<LanguageDropdown />
-					</li>
-					<li class="icon-slash">
-						<router-link to="/about">Qui sommes-nous ?</router-link>
-					</li>
-					<li class="icon-slash">
-						<RouterLink to="/conditions">Conditions Générales</RouterLink>
-					</li>
-					<li class="flex items-center"> <!-- Ajout de la classe "flex items-center" -->
-						<RouterLink to="/contact">Contact</RouterLink>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+    <div class="fixed flex flex-col top-0 w-screen bg-white z-10 shadow-md">
+        <div class="header-div justify-between flex flex-wrap py-4">
+            <router-link
+                to="/"
+                class="flex items-center gap-2 !text-black logo-font"
+            >
+                <div
+                    class="title-size font-bold underline font-yanone-kaffeesatz p-3 logo-size overflow-hidden flex items-center justify-center"
+                    style="top: -1rem"
+                >
+                    marauXsporTevenT
+                </div>
+            </router-link>
+            <div class="flex gap-8 items-center">
+                <ul id="header-menu" class="flex gap-4 button-header-size">
+                    <li class="icon-slash">
+                        <LanguageDropdown />
+                    </li>
+                    <li class="icon-slash">
+                        <router-link to="/about">Qui sommes-nous ?</router-link>
+                    </li>
+                    <li class="icon-slash">
+                        <RouterLink to="/conditions"
+                            >Conditions Générales</RouterLink
+                        >
+                    </li>
+                    <li class="icon-slash">
+                        <RouterLink to="/contact">Contact</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
-
 <script>
-import LanguageDropdown from './LanguageDropdown.vue';
+import LanguageDropdown from './LanguageDropdown.vue'
 
 export default {
-	components: {
-		LanguageDropdown
-	},
-};
+    components: {
+        LanguageDropdown,
+    },
+}
 </script>
-  
+
 <style lang="scss">
 .icon-slash {
-	position: relative;
-	display: flex;
-	align-items: center;
+    position: relative;
+    display: flex;
+    align-items: center;
 }
 
 .icon-slash::after {
-	content: "|";
-	margin-left: 10px;
-	/* Ajustez cette valeur pour contrôler l'espace entre l'icône et le texte */
-	color: #666;
-	font-family: Arial, Helvetica, sans-serif;
+    content: '|';
+    margin-left: 10px;
+    color: #666;
+    font-family: Arial, Helvetica, sans-serif;
 }
 
 .icon-slash:last-child::after {
-	content: none;
-	/* Supprime l'icône slash pour le dernier élément */
+    content: none;
+    /* Supprime l'icône slash pour le dernier élément */
 }
 </style>
--- frontend/src/components/LanguageDropdown.vue
+++ frontend/src/components/LanguageDropdown.vue
@@ -1,45 +1,44 @@
 <template>
     <div class="relative">
-      <button
-        @click="toggleDropdown"
-        class="bg-white text-gray-800 px-3 py-2 rounded-md shadow-sm focus:outline-none"
-      >
-        {{ selectedLanguage }}
-      </button>
-      <ul
-        v-if="showDropdown"
-        class="absolute right-0 mt-2 bg-white border rounded-md shadow-lg"
-      >
-        <li
-          v-for="(language, index) in languages"
-          :key="index"
-          @click="selectLanguage(language)"
-          class="cursor-pointer px-4 py-2 hover:bg-blue-500 hover:text-white"
+        <button
+            @click="toggleDropdown"
+            class="bg-white text-gray-800 px-3 py-2 rounded-md shadow-sm focus:outline-none"
         >
-          {{ language }}
-        </li>
-      </ul>
+            {{ selectedLanguage }}
+        </button>
+        <ul
+            v-if="showDropdown"
+            class="absolute right-0 mt-2 bg-white border rounded-md shadow-lg"
+        >
+            <li
+                v-for="(language, index) in languages"
+                :key="index"
+                @click="selectLanguage(language)"
+                class="cursor-pointer px-4 py-2 hover:bg-blue-500 hover:text-white"
+            >
+                {{ language }}
+            </li>
+        </ul>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     data() {
-      return {
-        showDropdown: false,
-        selectedLanguage: 'Français',
-        languages: ['Français', 'English', 'Español', 'Deutsch'],
-      };
+        return {
+            showDropdown: false,
+            selectedLanguage: 'FR',
+            languages: ['FR', 'EN', 'ES', 'DE'],
+        }
     },
     methods: {
-      toggleDropdown() {
-        this.showDropdown = !this.showDropdown;
-      },
-      selectLanguage(language) {
-        this.selectedLanguage = language;
-        this.showDropdown = false;
-      },
+        toggleDropdown() {
+            this.showDropdown = !this.showDropdown
+        },
+        selectLanguage(language) {
+            this.selectedLanguage = language
+            this.showDropdown = false
+        },
     },
-  };
-  </script>
-  
+}
+</script>
